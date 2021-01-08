471727a8830dba0e757e3738de4f45266504324b
Astrov <vpopovic003@gmail.com>
2021-01-08 01:22:17 +0100

    code

--- C/Udemy/58-InitializationArrays.c
+++ C/Udemy/58-InitializationArrays.c
@@ -5,5 +5,11 @@ int main()
   int sample[100] = {1}; //samo prvi element definisan, svi ostalo automatski 0
   for(int i = 0; i < 100; ++i)
     printf("%d\n", sample[i]);
+
+    printf("\n");
+
+  int sample2[100] = {[2]=4234, [69]=2432}; //dodeljivanje pojedinacnog elementa
+  for(int j = 0; j < 100; ++j)
+    printf("%d\n", sample2[j]);
   return 0;
 }
