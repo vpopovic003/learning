68574f91a396c2a6f340bbd9ad91d1b78348eab5
Astrov <vpopovic003@gmail.com>
2021-01-31 17:58:14 +0100

    code

---
+++ C/Udemy/61-CH-generate-prime-numbers.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+
+int main(){
+    int primes[100] = {1, 2};
+
+    int i = 3;
+    for (i = 3; i <= 100; i++){
+
+        int x = 0;
+        for (x = 0; x <= i; x ++){
+            if (i % primes[x] = 0)
+                
+        }
+        printf("\n%d\n", primes[x]);
+    }
+
+    return 0;
+}
